83be9273ac8c00c93c4302a1794d6e6185edc680
Tokyodays <kikuchi@gmail.com>
2015-02-21 12:33:56 +0900

    add carousel

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,6 @@
 
 (function($) {
   $("#PICK_UP > div").flatHeights();
-
-  $(".camera_wrap").camera({
-    height: '345px',
-    loader: 'pie',
-    pagination: true,
-    thumbnails: false
-  });
+  $('.carousel').carousel();
+  
 })(jQuery);
