f20802f815fd622f18759d247024e6ede16f86a7
Tokyodays <kikuchi@gmail.com>
2015-03-13 23:18:07 +0900

    update

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,21 @@
 (function($) {
   $("#PICK_UP > div").flatHeights();
   $('.carousel').carousel();
-  
+      $('.drawer').drawer();
+
+    $('.drawer-api-toggle').on(function(){
+      $('.drawer').drawer("open");
+    });
+
+    $('.drawer').on('drawer.opened',function(){
+      console.log('opened');
+    });
+
+    $('.drawer').on('drawer.closed',function(){
+      console.log('closed');
+    });
+
+    $('.drawer-dropdown-hover').hover(function(){
+      $('[data-toggle="dropdown"]', this).trigger('click');
+    });
 })(jQuery);
